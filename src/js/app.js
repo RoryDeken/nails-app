f9834bdf309009ae5d279f04a79ed95e84d57758
Rory Deken <deken.7@osu.edu>
2018-02-12 15:26:09 -0500

    updated data organization

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,19 @@
 // Place JS here.
 var previewer = new Vue({
-  el: '#previewer',
+  el: "#previewer",
   data: {
-    url: 'http://placehold.it/500x750'
+    url: "http://placehold.it/500x750",
+    collections: {
+        collectionOne: {
+          skinOne: {
+            shapeOne: {
+              sizeOne: {
+                colors: ["url1", "url2"]
+              } //end sizeOne
+            } //end shapeOne
+          } //end skinOne
+        } // end collectionOne
+      } //end collections
+    
   }
-})
+});
