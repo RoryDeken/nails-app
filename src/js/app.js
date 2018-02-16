288be016dbcaaea89c4cc8217c947cb9e5f76cf4
Rory Deken <deken.7@osu.edu>
2018-02-16 16:20:17 -0500

    added computed values to DOM

--- src/js/app.js
+++ src/js/app.js
@@ -2,18 +2,34 @@
 var previewer = new Vue({
   el: "#previewer",
   data: {
-    url: "http://placehold.it/500x750",
-    collections: {
-        collectionOne: {
-          skinOne: {
-            shapeOne: {
-              sizeOne: {
-                colors: ["url1", "url2"]
-              } //end sizeOne
-            } //end shapeOne
-          } //end skinOne
-        } // end collectionOne
-      } //end collections
-    
-  }
+    skins: ['http://via.placeholder.com/500x500','http://via.placeholder.com/300x300'],
+    collections:
+        [ //nailLength
+            [ //shape array
+              [ // size array
+                ["http://via.placeholder.com/350x150", "http://via.placeholder.com/550x150"] // colors
+              ]
+            ] //end shapeOne
+        ],// end collections
+
+        values: {
+          collection: 0,
+          nailLength: 0,
+          skin:0,
+          shape: 0,
+          color: 0},
+  },
+    computed: {
+      url: function(){
+        nail = this.collections[this.values.collection][this.values.nailLength][this.values.shape][this.values.color] ? this.collections[this.values.collection][this.values.nailLength][this.values.shape][this.values.color] : nail;
+        console.log(nail);
+        return nail;
+      },
+      skin: function(){
+        hand = this.skins[this.values.skin];
+
+        return hand;
+      }
+
+    }
 });
