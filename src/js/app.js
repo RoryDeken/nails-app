9ee434eab6c58bcf77a8c29b87a4edc96a86fb82
Rory Deken <deken.7@osu.edu>
2018-02-17 10:05:04 -0500

    added main functions and test assets

--- src/js/app.js
+++ src/js/app.js
@@ -2,12 +2,12 @@
 var previewer = new Vue({
   el: "#previewer",
   data: {
-    skins: ['http://via.placeholder.com/500x500','http://via.placeholder.com/300x300'],
+    hand: '../dist/img/hand.png',
     collections:
         [ //nailLength
             [ //shape array
               [ // size array
-                ["http://via.placeholder.com/350x150", "http://via.placeholder.com/550x150"] // colors
+                ["http://via.placeholder.com/350x150", "http://via.placeholder.com/550x150"]
               ]
             ] //end shapeOne
         ],// end collections
@@ -15,21 +15,24 @@ var previewer = new Vue({
         values: {
           collection: 0,
           nailLength: 0,
+          hand:0,
           skin:0,
           shape: 0,
-          color: 0},
+          color: '#ebcec0'},
   },
     computed: {
+
+      /*
       url: function(){
-        nail = this.collections[this.values.collection][this.values.nailLength][this.values.shape][this.values.color] ? this.collections[this.values.collection][this.values.nailLength][this.values.shape][this.values.color] : nail;
+      //  nail = this.collections[this.values.collection][this.values.nailLength][this.values.shape][this.values.color] ? this.collections[this.values.collection][this.values.nailLength][this.values.shape][this.values.color] : nail;
         console.log(nail);
         return nail;
       },
       skin: function(){
-        hand = this.skins[this.values.skin];
+        hand = this.hands[this.values.hand];
 
         return hand;
       }
-
+      */
     }
 });
