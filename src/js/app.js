52cfa966e5e61ce06eb40019ef3501c1aacc30e3
Rory Deken <deken.7@osu.edu>
2018-02-26 11:52:16 -0500

    updated hand rendering and added loading styles

--- src/js/app.js
+++ src/js/app.js
@@ -2,37 +2,22 @@
 var previewer = new Vue({
   el: "#previewer",
   data: {
-    hand: '../dist/img/hand.png',
-    collections:
-        [ //nailLength
-            [ //shape array
-              [ // size array
-                ["http://via.placeholder.com/350x150", "http://via.placeholder.com/550x150"]
-              ]
-            ] //end shapeOne
-        ],// end collections
-
-        values: {
+          loading: true,
           collection: 0,
           nailLength: -15,
           hand:0,
           skin:1,
           shape: 0,
           color: '#ebcec0'},
-  },
+
     computed: {
-
-      /*
-      url: function(){
-      //  nail = this.collections[this.values.collection][this.values.nailLength][this.values.shape][this.values.color] ? this.collections[this.values.collection][this.values.nailLength][this.values.shape][this.values.color] : nail;
-        console.log(nail);
-        return nail;
-      },
-      skin: function(){
-        hand = this.hands[this.values.hand];
-
-        return hand;
-      }
-      */
+        skin: function(){
+          return this.skin;
     }
+  },
+  mounted: function(){
+    setTimeout(function(){
+      previewer.loading = false;
+    }, 1000);
+  }
 });
