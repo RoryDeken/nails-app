434fe6c43dd8f132cfbce00be232d5ece6f590f5
Rory Deken <deken.7@osu.edu>
2018-02-28 12:06:28 -0500

    added new images and improved performance

--- src/js/app.js
+++ src/js/app.js
@@ -2,13 +2,29 @@
 var previewer = new Vue({
   el: "#previewer",
   data: {
+    fakeUrls: [
+
+          './dist/img/fake.png',
+          './dist/img/fake.png',
+          './dist/img/fake.png',
+          './dist/img/fake.png',
+          './dist/img/fake.png'
+        ],
+          realUrls:  [
+
+                    './dist/img/2.png',
+                    './dist/img/3.png',
+                    './dist/img/4.png',
+                    './dist/img/5.png',
+                    './dist/img/6.png',
+                  ],
           loading: true,
           collection: 0,
           nailLength: -15,
           hand:0,
           skin:1,
           shape: 0,
-          color: '#ffc3e5',
+          color: '255, 195, 229',
 
         },
 
@@ -16,10 +32,22 @@ var previewer = new Vue({
         skin: function(){
           return this.skin;
     }
+
+  },
+  methods: {
+    fakeUrl: function (index) {
+      return this.fakeUrls[index];
+    }
   },
   mounted: function(){
     setTimeout(function(){
       previewer.loading = false;
-    }, 2500);
-  }
+    }, 3000);
+    setTimeout(function(){
+      console.log('replacing images');
+      for(i=0; i < previewer.fakeUrls.length; i++){
+        previewer.fakeUrls[i] = previewer.realUrls[i];
+      }
+    }, 5000);
+  },
 });
